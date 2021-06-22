18461934e6d4a6427b89685b3cd83e7b54630592
Serova Elena <e.serova1706@gmail.com>
2021-06-22 17:59:06 +0300

    palindrome

---
+++ js/script3.js
@@ -0,0 +1,30 @@
+function checkPalindrome(str) {
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1] - i) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+  function testWord(word, isTrue) {
+    if (checkPalindrome(word) === isTrue) {
+      console.log('палиндром');
+    } else {
+      console.log('не палиндром');
+    }
+  }
+
+
+function testPalindrome() {
+  testWord('шалаш', true);
+  testWord('level', true);
+  testWord('пляж', false);
+  }
+
+testPalindrome();
+  
+/*   checkPalindrome('шалаш');
+  checkPalindrome('шалашик');
+  checkPalindrome('оео'); */
